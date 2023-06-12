1f009ca36b7e830747610cd65ac9b64adb0f9575
Marco <79325830+Marco2124@users.noreply.github.com>
2023-06-12 16:20:59 +0200

    Drop xp if mined without silk touch

--- src/main/java/me/marco2124/minespawners/mixins/SpawnerBlockDropsXpMixin.java
+++ src/main/java/me/marco2124/minespawners/mixins/SpawnerBlockDropsXpMixin.java
@@ -1,14 +1,21 @@
 package me.marco2124.minespawners.mixins;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SpawnerBlock;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(SpawnerBlock.class)
 public class SpawnerBlockDropsXpMixin {
-    @ModifyVariable(method = "onStacksDropped", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    private boolean injected(boolean dropExperience) {
-        return false;
+    @Inject(method = "onStacksDropped", at = @At("HEAD"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void injected(BlockState state, ServerWorld world, BlockPos pos, ItemStack tool, boolean dropExperience, CallbackInfo ci) {
+        if (EnchantmentHelper.hasSilkTouch(tool)) ci.cancel();
     }
 }
