9425eed82d3aa1041bca58393081e54e5b622cb4
Marco <79325830+Marco2124@users.noreply.github.com>
2023-09-26 00:15:54 +0200

    Updated to 1.20.2
    Should work on 1.20, 1.20.1 and 1.20.2 now

--- src/main/java/me/marco2124/minespawners/Mine_spawners.java
+++ src/main/java/me/marco2124/minespawners/Mine_spawners.java
@@ -3,7 +3,6 @@ package me.marco2124.minespawners;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.block.Blocks;
-import net.minecraft.loot.LootPool;
 import net.minecraft.util.Identifier;
 
 public class Mine_spawners implements ModInitializer {
@@ -12,16 +11,9 @@ public class Mine_spawners implements ModInitializer {
     private static final Identifier SPAWNER_LOOT_TABLE_ID = Blocks.SPAWNER.getLootTableId();
     @Override
     public void onInitialize() {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if (source.isBuiltin() && SPAWNER_LOOT_TABLE_ID.equals(id)) {
-                LootPool[] pools = lootManager.getLootTable(new Identifier(MOD_ID, path)).pools;
-                if (pools != null) {
-                    System.out.println(pools[0].entries.toString());
-                    for (LootPool pool : pools) {
-                        tableBuilder.pool(pool);
-                    }
-                }
-            }
-        });
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, tableBuilder, source) ->
+                source.isBuiltin() && SPAWNER_LOOT_TABLE_ID.equals(id) ?
+                lootManager.getLootTable(new Identifier(MOD_ID, path)) :
+                null);
     }
 }
