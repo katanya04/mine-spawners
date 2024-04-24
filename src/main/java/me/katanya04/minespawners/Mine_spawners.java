b179a12b91b7387f20537127031417c003ea7aba
Marco2124 <katanya024@gmail.com>
2024-04-24 23:07:01 +0200

    Updated to 1.20.5

--- src/main/java/me/katanya04/minespawners/Mine_spawners.java
+++ src/main/java/me/katanya04/minespawners/Mine_spawners.java
@@ -1,19 +1,40 @@
 package me.katanya04.minespawners;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.MobSpawnerBlockEntity;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.tag.ItemTags;
 
 public class Mine_spawners implements ModInitializer {
-    public static final String MOD_ID = "mine-spawners";
-    private static final String path = "spawner";
-    private static final Identifier SPAWNER_LOOT_TABLE_ID = Blocks.SPAWNER.getLootTableId();
     @Override
     public void onInitialize() {
-        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, tableBuilder, source) ->
-                source.isBuiltin() && SPAWNER_LOOT_TABLE_ID.equals(id) ?
-                        lootManager.getLootTable(new Identifier(MOD_ID, path))
-                        : null);
+        PlayerBlockBreakEvents.AFTER.register((world, player, blockPos, blockState, blockEntity) -> {
+            if (blockEntity == null || !blockEntity.getType().equals(BlockEntityType.MOB_SPAWNER))
+                return;
+            MobSpawnerBlockEntity spawnerBlock = (MobSpawnerBlockEntity) blockEntity;
+            ItemStack itemInHand = player.getStackInHand(player.getActiveHand());
+            if (!itemInHand.isIn(ItemTags.PICKAXES) || !EnchantmentHelper.hasSilkTouch(itemInHand))
+                return;
+            ItemStack spawnerStack = new ItemStack(Items.SPAWNER, 1);
+            if (spawnerBlock.getLogic().getRenderedEntity(world, blockPos) != null) {
+                NbtCompound nbt = new NbtCompound();
+                spawnerBlock.getLogic().writeNbt(nbt);
+                nbt.putString("id", "minecraft:mob_spawner");
+                nbt.putShort("Delay", (short) -1);
+                ComponentMap components = ComponentMap.builder().add(DataComponentTypes.BLOCK_ENTITY_DATA, NbtComponent.of(nbt)).build();
+                spawnerStack.applyComponentsFrom(components);
+            }
+            ItemEntity spawnerItem = new ItemEntity(world, blockPos.getX() + 0.5f, blockPos.getY() + 0.5f, blockPos.getZ() + 0.5f, spawnerStack);
+            world.spawnEntity(spawnerItem);
+        });
     }
 }
