1f6358f2401eda604f4dfc45a00a3a705b50cf3e
katanya04 <katanya024@gmail.com>
2025-03-19 23:54:39 +0100

    Added javadoc for new mixin

--- src/main/java/me/katanya04/minespawners/mixins/TrialSpawnerCorrectToolMixin.java
+++ src/main/java/me/katanya04/minespawners/mixins/TrialSpawnerCorrectToolMixin.java
@@ -6,6 +6,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+/**
+ * Make trial spawners drop only when mined with required tool (pickaxe)
+ */
 @Mixin(Blocks.class)
 public class TrialSpawnerCorrectToolMixin {
     @ModifyArg(at = @At(value = "INVOKE", target = "net/minecraft/block/Blocks.register (Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;", ordinal = 825), method = "<clinit>", index = 2)
