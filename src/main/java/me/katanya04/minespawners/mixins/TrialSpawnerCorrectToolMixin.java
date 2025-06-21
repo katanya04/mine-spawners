9ad4f30d3512a3df45fbc30a0d0efb3fbae4d3ac
katanya04 <katanya024@gmail.com>
2025-06-21 17:16:04 +0200

    Updated to 1.21.6

--- src/main/java/me/katanya04/minespawners/mixins/TrialSpawnerCorrectToolMixin.java
+++ src/main/java/me/katanya04/minespawners/mixins/TrialSpawnerCorrectToolMixin.java
@@ -1,17 +1,24 @@
 package me.katanya04.minespawners.mixins;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 /**
  * Make trial spawners drop only when mined with required tool (pickaxe)
  */
 @Mixin(Blocks.class)
 public class TrialSpawnerCorrectToolMixin {
-    @ModifyArg(at = @At(value = "INVOKE", target = "net/minecraft/block/Blocks.register (Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;", ordinal = 850), method = "<clinit>", index = 2)
+
+    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE",
+        target = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;"
+        ), slice = @Slice(
+                from = @At(value = "CONSTANT", args = "stringValue=trial_spawner"),
+                to = @At(value = "CONSTANT", args = "stringValue=vault")
+        ), index = 2
+    )
     private static AbstractBlock.Settings injected(AbstractBlock.Settings settings) {
         return settings.requiresTool();
     }
